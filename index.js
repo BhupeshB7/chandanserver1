3669d47a7f52d3d7232f95353be935d1a80bb045
Bhupesh Kumar <bhupeshkr2912@gmail.com>
2023-09-09 01:09:00 +0530

    update

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ const { v4: uuidv4 } = require('uuid');
 const passwordRoute = require('./routes/passwordReset');
 const register = require('./routes/register');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5500;
 
 // Database setup (use your MongoDB connection string)
 mongoose.connect(process.env.MONGO_URL, {
